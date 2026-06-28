cfc696adeca07f199f6dc84cf7f78e6305145226
Muhammad Anas <daggaanas0888@gmail.com>
2026-06-28 18:30:31 +0530

    add remove button for books

--- script.js
+++ script.js
@@ -6,13 +6,28 @@ const mainArea = document.querySelector(".main");
 button.addEventListener("click",function() {
 const myDiv = document.createElement('div');
 myDiv.textContent = "hello i am an new book";
- mainArea.appendChild(myDiv);
- event.preventDefault();
+// mainArea.appendChild(myDiv);
+// event.preventDefault();
 
 myDiv.style.width = "200px";
 myDiv.style.height = "200px";
 myDiv.style.border = "2px solid black";
 myDiv.style.margin = "10px";
+
+const removeBtn = document.createElement('button');
+    removeBtn.textContent = "Remove";
+    removeBtn.style.marginTop = "10px";
+    removeBtn.style.backgroundColor = "#ff4d4d";
+    removeBtn.style.color = "white";
+    removeBtn.style.border = "none";
+    removeBtn.style.padding = "5px 10px";
+    removeBtn.style.cursor = "pointer";
+
+
+   removeBtn.addEventListener('click' , function(){
+myDiv.remove();
+       });
+ //  myDiv.appendChild(removeBtn);
 
 const nameOfBook = document.getElementById('bookName').value
 const authorName = document.getElementById('authorName').value;
@@ -27,6 +42,9 @@ myDiv.innerHTML = `<p>Name: ${nameOfBook} </p> <br>
 <p> Status: ${bookStatus} </p>
 
 `
+   myDiv.appendChild(removeBtn);
 
+ mainArea.appendChild(myDiv);
+ event.preventDefault();
 });
 
